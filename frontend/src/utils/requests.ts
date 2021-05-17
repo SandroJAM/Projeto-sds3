1aaba143ec0fdfe5986f7ee9794a2185d8592c6a
SandroJAM <sandro.mnz@gmail.com>
2021-05-17 15:39:30 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,7 @@
-export const BASE_URL = 'http://localhost:8080';
+// Conexão LOCAL
+
+// export const BASE_URL = 'http://localhost:8080';
+
+// Conexão com Netlify e Heroku
+
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
